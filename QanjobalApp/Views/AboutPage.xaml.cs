6d9a5ccf7ede703539cf8497db64b6adf264034b
Ismael Cuevas <ijcuevas2@gmail.com>
2018-02-09 14:50:34 -0600

    first commit

---
+++ QanjobalApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace QanjobalApp
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
